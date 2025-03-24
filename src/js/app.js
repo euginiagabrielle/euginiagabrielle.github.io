f6192ddbc10b5da955ba1a17c56e1e29f566df23
euginiagabrielle <168151693+euginiagabrielle@users.noreply.github.com>
2025-03-24 08:41:57 +0700

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+          const container = document.getElementById('posts-container');
+
+          posts.forEach(post => {
+              const card = document.createElement('div');
+              card.classList.add('card');
+              card.style.width = "18rem";
+              card.innerHTML = `
+                  <div class="card-body text-center">
+                      <h5 class="card-title">${post.title}</h5>
+                      <p class="card-text">${post.body}</p>
+                  </div>
+                  <hr>
+                  <p class="post-id text-center">Post ID: ${post.id}</p>
+              `;
+              container.appendChild(card);
+          });
+      })
+      .catch(error => console.error('Error fetching posts:', error));
+});
